b547365e2ec94b9be419482bcb7f023a02daa36b
saisubodh_MSFT <saisubodh@microsoft.com>
2022-01-31 22:53:29 +0530

    WordPress: Debug DB connection wait

--- GenerateDockerFiles/wordpress/wordpress/local_bin/entrypoint.sh
+++ GenerateDockerFiles/wordpress/wordpress/local_bin/entrypoint.sh
@@ -99,7 +99,7 @@ setup_wordpress() {
 	while [ $try_count -le 40 ]
 	do 
 	    db_status=`wp db check --allow-root --quick --ssl=true|grep "Success: Database checked"|wc -l`
-	    if(( $db_status==1 ))
+	    if(( $db_status>=1 ))
 	    then
 		echo "INFO: Database Connection Successful "            
 		break
@@ -109,8 +109,8 @@ setup_wordpress() {
 	    fi
 	    let try_count+=1
 	done
+    	echo "DB_CONNECTION_ESTABLISHED" >> $WORDPRESS_LOCK_FILE
     fi
-    echo "DB_CONNECTION_ESTABLISHED" >> $WORDPRESS_LOCK_FILE
     
     if [ $(grep "DB_CONNECTION_ESTABLISHED" $WORDPRESS_LOCK_FILE) ] &&  [ ! $(grep "WP_INSTALLATION_COMPLETED" $WORDPRESS_LOCK_FILE) ]; then
         if wp core install --url=$WEBSITE_HOSTNAME --title="${WORDPRESS_TITLE}" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --path=$WORDPRESS_HOME --allow-root; then
